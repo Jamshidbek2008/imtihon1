e622c4ecdc15d970d6bf274990d261db17b7c616
Jamshidbek2008 <jamshidbek00789@gmail.com>
2023-08-25 12:31:48 +0500

    imtihon

---
+++ app.js
@@ -0,0 +1,56 @@
+// let r,l,o,p,q;
+// function myfuntion(n) {
+//     r=n.toString;
+//     q='';
+//     l=r[0];
+//     o=r[length];
+//     for(let i=1; i<r.length-1, i++){
+//       console.log(`${r[i]}`);
+//     }
+// }
+// myfuntion(6789);
+
+
+
+
+
+
+function myfuntion(oy){
+    if(oy==1 || oy==2 || oy==3){
+        console.log("qish");
+    }
+    else if(oy==4 || oy==5 ||oy==6){
+        console.log("bahor");
+    }
+    else if(oy==7 || oy==8 || oy==9){
+        console.log("yoz");
+    }
+    else{
+        console.log("kuz");
+    }
+}
+myfuntion(6);
+
+
+
+
+
+
+// let e;
+// let u;
+// let r;
+// function onlik(a){
+//     e=a%10;
+//     u=((a-e)/10)%10;
+//     console.log(`${u}`);
+// }
+// onlik(1269);
+
+
+
+
+// bir ishni bir necha marta qo'lashda functiondan foydalanamiz
+// function name(params) {
+    
+// }
+//kabi yaratiladi
